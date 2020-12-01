c3e9aae8b62c004c3d912539cd42a8bb93cdc911
Полина <vaganowa.p@yandex.ru>
2020-12-01 13:59:27 +0300

    'third commit'

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -133,6 +133,6 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATIC_ROOT = 'staticfiles'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
